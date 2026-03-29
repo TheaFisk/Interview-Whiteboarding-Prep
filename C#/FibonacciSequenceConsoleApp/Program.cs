2455bb99801d803cdbf0a6fbe57d59d566a23cf1
TheaFisk <theafisk@gmail.com>
2026-03-28 19:40:39 -0600

    Commit

--- C#/FibonacciSequenceConsoleApp/Program.cs
+++ C#/FibonacciSequenceConsoleApp/Program.cs
@@ -11,8 +11,8 @@ class Program
 {
     static void Main()
     {
-        int CurrentNumber = 3;
-        Console.WriteLine($"Setup {CurrentNumber}");
+        //int CurrentNumber = 3;
+        //Console.WriteLine($"Setup {CurrentNumber}");
 
         // Let's think this through. The Fibonacci Sequence is the prior number + Current number = future number
         // 1 (1), 2 (1+1), 3 (2+1), 5 (3+2), 8 (5+3), 13 (8+5)
@@ -20,7 +20,7 @@ class Program
         //So, we need three variables. A previous number, a current number, and a future number. 
         //We need to loop through one process 'n' times
 
-        Console.WriteLine("What is the nth number you want to reach? ");
+        Console.Write("What is the nth number you want to reach? ");
         //int n = Console.Read(); 
         int n = int.Parse(Console.ReadLine());
         int loop = 0;
@@ -28,11 +28,13 @@ class Program
         int current = 1;
         int next = 1;
 
+        Console.WriteLine("Sequence");
+        Console.WriteLine("- - - - - - - - ");
         while (loop <= n)
         {
                 
             loop = loop + 1;
-            Console.WriteLine($"Number is {next}");   
+            Console.WriteLine($"{next}");   
 
             prior = current;
             current = next;
--- C#/FibonacciSequenceConsoleApp/obj/Debug/net9.0/FibonacciSequenceConsoleApp.AssemblyInfo.cs
+++ C#/FibonacciSequenceConsoleApp/obj/Debug/net9.0/FibonacciSequenceConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FibonacciSequenceConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ddbb004956d75dbe0624836523f2035ccad82e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79531b3603a3f43a50c2a647d0ac669c8af1c52f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FibonacciSequenceConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FibonacciSequenceConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
