9df6ffe42917e10961e36dd1dd6e945822852a07
TheaFisk <theafisk@gmail.com>
2026-03-29 22:38:04 -0600

    C tests

---
+++ C/Fibonacci.c
@@ -0,0 +1,20 @@
+
+int main()
+{
+    //Fibonacci Sequence in C
+
+    int n;
+    printf("Nth number");
+    scanf("%d", &n);
+    
+    int loop;
+    double prior;
+    double current;
+    double next;
+
+    while (n > loop)
+    {
+        printf(next);
+    }
+    return 0;
+}
