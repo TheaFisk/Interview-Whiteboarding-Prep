29b619586c096a0e778c731d4c313de493173071
TheaFisk <theafisk@gmail.com>
2026-03-29 22:24:02 -0600

    commit

--- C#/LinkedList/Program.cs
+++ C#/LinkedList/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+//Linked list ???
+
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Linked List is just a bumch of Nodes linked together
+
+            // First, you need a Node class. Nodes consist of a Data type and a pointer.
+
+            public class Node
+            {
+                public Node next;
+                public Object data;
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
+
