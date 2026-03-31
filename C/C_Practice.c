8f54e1fd8904f96f5f6ff336a5da35fb33f04023
TheaFisk <theafisk@gmail.com>
2026-03-30 22:44:06 -0600

    C Practice Update Day 2

--- C/C_Practice.c
+++ C/C_Practice.c
@@ -6,8 +6,54 @@ int main()
     //int n;
     //scanf("%d", &n);
 
-    //printf("%d\n", n);
+    /*
+    
+    if n == 1;
+        //First case
+    else if n == 2;
+        //Second case
 
-    char n = "Hello World";
-    printf(n);
+    else if n == 3;
+        //Third case
+
+    //printf("%d\n", n); 
+
+    char n = 'Hello World';
+    printf("%c\n", n);*/
+
+    // Create Variables
+
+    char wordOne = 'l';
+
+    printf("%c\n", wordOne);
+
+
+    /*
+    // Create variables
+    int myNum = 15;            // Integer (whole number)
+    float myFloatNum = 5.99;   // Floating point number
+    char myLetter = 'D';       // Character
+
+    // Print variables
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+*/
+
+    // Multiple variables
+
+    int x = 1, y = 2, z = 3;
+    int a, b, c;
+    a = b = c = 12;
+
+    // Basic Calculation
+
+    int width = 5;
+    int height = 4;
+
+    int area;
+    area = width * height;
+
+    printf("Area of the rectangle is: %d", area);
+
 }
