e08c88b438ff9aabbb3a69f6585bcd0459c68756
TheaFisk <theafisk@gmail.com>
2026-03-30 22:19:51 -0600

    So many struggles in C

--- C/C_Practice.c
+++ C/C_Practice.c
@@ -3,8 +3,11 @@
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
+    //int n;
+    //scanf("%d", &n);
 
-    printf("%d\n", n);
+    //printf("%d\n", n);
+
+    char n = "Hello World";
+    printf(n);
 }
